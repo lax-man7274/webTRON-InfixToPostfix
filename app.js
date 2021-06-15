eb08385bb3e1c2d3f426332221d3d749d9a2a020
Laxman Pokhrel <laxmanpokhrel102@gmail.com>
2021-06-15 21:46:37 +0545

    PORT added

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-const express = require('express');
-const compression = require('compression');
-const path = require('path');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const flash=require('connect-flash');
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const flash = require("connect-flash");
 
 // const csrf = require('csurf');
 
-const serverRoutes = require('./routes/serverRoutes');
-const errorHandler = require('./middleware/errorHandler');
-const mongoConnect = require('./utils/database').mongoConnection;
+const serverRoutes = require("./routes/serverRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const mongoConnect = require("./utils/database").mongoConnection;
 
 const app = express();
 
 const store = new MongoDBStore({
-    uri: process.env.MONGO_URI,
-    collection: 'mySessions'
-  });
-
+  uri: process.env.MONGO_URI,
+  collection: "mySessions",
+});
 
 app.use(compression());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(session({
-    secret: 'secretKey',
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(
+  session({
+    secret: "secretKey",
     resave: false,
     saveUninitialized: false,
-    store:store
-}));
+    store: store,
+  })
+);
 app.use(flash());
 app.use(serverRoutes);
-
 
 app.use(errorHandler.pageNotFoundError);
 app.use(errorHandler.serverError);
 // const PORT=process.env.PORT || 4000;
 mongoConnect(() => {
-    app.listen(4000, () => {
-        console.log('listening to PORT: ', 4000);
-    });
+  app.listen(process.env.PORT || 4000, () => {
+    console.log("listening to PORT: ", 4000);
+  });
 });
