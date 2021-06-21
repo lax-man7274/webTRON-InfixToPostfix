47eadbcb0021c5ee8a066202f45ca71b308cc74b
Pramesh Karki <Parameswarkarki@gmail.com>
2021-06-21 22:28:02 +0545

    Spinner was added and UI Improved

---
+++ public/js/client.js
@@ -0,0 +1,9 @@
+const spinnerWrapper=document.getElementById("spinner-wrapper");
+const gitLink=document.querySelector(".git-link");
+
+window.addEventListener("load",()=>{
+     spinnerWrapper.style.display="none";
+     document.body.style.overflowY="scroll";
+     gitLink.style.display="block";
+
+})
